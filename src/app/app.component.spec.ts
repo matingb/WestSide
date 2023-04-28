57ef9996ed5dd54cbbb0d24edcaf677e68a58492
Matías García <mgarcia@inrule.com>
2023-04-28 14:21:13 -0300

    agrega prueba que valide el html

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -3,6 +3,8 @@ import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 import { Dia } from './models/dia.model';
 import { ClassDetailsComponent } from './components/class-details/class-details.component';
+import { Clase } from './models/clases.model';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
 
@@ -33,6 +35,7 @@ describe('AppComponent', () => {
   });
 
   it('debe retornar el Dia Lunes si el dia actual es lunes', () => {
+    dadoDiasConClases();
     const fechaMock = new Date(2022, 5, 20); // lunes
     spyOn(Date, 'now').and.returnValue(fechaMock.getTime());
     
@@ -40,4 +43,28 @@ describe('AppComponent', () => {
 
     expect(dia.nombre).toBe("Lunes");
   });
+
+  it('debe mostrar las clases del dia Lunes si el dia actual es lunes', () => {
+    component.diaActual = lunes();
+    fixture.detectChanges();
+
+    const clases = fixture.debugElement.queryAll(By.css("gym-class-details"));
+
+    expect(clases).toHaveSize(1);
+  });
+
+  function dadoDiasConClases() {
+    component.dias = [
+      lunes(),
+      new Dia("Martes", [])
+    ];
+    fixture.detectChanges();
+  }
+
+  function lunes(): Dia {
+    return new Dia("Lunes", 
+    [
+      new Clase("10:00", "Calistenia", [])
+    ]) 
+  }
 });
